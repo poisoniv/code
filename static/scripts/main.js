926ce2b594e1c25c0b8a7a7a686b61caaedff82c
Tom Elliott <tmelliottjr@gmail.com>
2017-01-26 19:23:41 -0500

    stuff and things

---
+++ static/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // initialize autocomplete
+    $( ".auto" ).autocomplete({
+        source: [[ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]]
+    });
+
+});

1def8a91953757cad4393b48d263a2f2918c60ca
Tom Elliott <tmelliottjr@gmail.com>
2017-01-26 22:32:04 -0500

    autocomplete

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 
-    // initialize autocomplete
-    $( ".auto" ).autocomplete({
-        source: [[ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]]
-    });
+$(".auto").autocomplete({
+    valueKey: 'title',
+    appendMethod: 'replace',
+    source: [function(q, add) {
+        $.getJSON("query?s=" + encodeURIComponent(q), function(resp) {
+            console.log(resp);
+            add(resp);
+        })
+    }]
+});
 
 });
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from models import product
 from google.appengine.ext import ndb
 import logging
@@ -9,28 +9,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
-	
-
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/product_query")
 def product_query():
-    # This will be the endpoint for querying GCP DataStore
-    # This will take the results from the query and create the JSON needed for the text box
-	
-	    # This is main page of the site
-	qry = product.query(product.productname >= "babe")
-	logging.info("products:")
-	logging.info(qry)
-	tommy="2peas"
-	payload = []
-	for prd in qry:		
-		payload.append(prd.productname)
-		
-		logging.info(prd.productname)
-	return json.dumps(payload)
+    search_term = request.args.get('s')
+    qry = product.query(product.productname >= search_term)
+
+    logging.info("products:")
+    logging.info(qry)
+
+    payload = []
+
+    for prd in qry:
+        payload.append(prd.productname)
+        logging.info(prd.productname)
+        data = {"title": prd.productname}
+        payload.append(data)
+
+    return json.dumps(payload)
 
 
 if __name__ == "__main__":
